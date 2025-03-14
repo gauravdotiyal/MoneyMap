192918801d90ee4aadefae8658130cf052ed4e25
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2025-03-14 23:35:14 +0530

    Dashboard Form with zod Validation

---
+++ actions/dashboard.js
@@ -0,0 +1,70 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+const serializeTransaction = (obj) => {
+  const serialized = { ...obj };
+
+  if (obj.balance) {
+    serialized.balance = obj.balance.toNumber();
+  }
+};
+
+export async function createAccount(data) {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+      where: {
+        clerkUserId: userId,
+      },
+    });
+
+    if (!user) {
+      throw new Error("User Not Found");
+    }
+
+    // Convert Balance into float before
+    const balanceFloat = parseFloat(data.balance);
+    if (isNan(balanceFloat)) {
+      throw new Error("Invalid Balance Amount");
+    }
+
+    const existingAccount = db.account.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    const shouldDefault = existingUser.length === 0 ? true : data.isDefault;
+    //If the account we have is default set the others account as unset for default
+    if (shouldDefault) {
+      await db.account.updateMany({
+        where: {
+          userId: user.id,
+          isDefault: true,
+        },
+        data: {
+          isDefault: false,
+        },
+      });
+    }
+
+    const account = await db.account.create({
+      data: {
+        ...data,
+        balance: balanceFloat,
+        userId: user.id,
+        isDefault: shouldDefault,
+      },
+    });
+
+    const serializedAccount = serializeTransaction(account);
+    revalidatePath("/dashboard");
+    return { success: true, data: serializedAccount };
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
