bb7db38422de71d6d61c70719d43ab281c40819d
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2025-04-18 00:00:32 +0530

    checking commit

---
+++ emails/template.jsx
@@ -0,0 +1,107 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+
+export default function EmailTemplate({
+  userName = "",
+  type = "budget-alert",
+  data = {},
+}) {
+  if (type === "monthly-report") {
+  }
+  if (type === "budget-alert") {
+    return (
+      <Html>
+        <Head />
+        <Preview>Budget Alert</Preview>
+        <Body styles={styles.body}>
+          <Container styles={styles.container}>
+            <Heading styles={styles.heading}>Budget Alert</Heading>
+            <Text styles={styles.text}>Hello {userName}, </Text>
+            <Text styles={styles.text}>
+              You&rsquo;ve used {data?.percentageUsed.toFixed(1)}% of your
+              monthly budget.
+            </Text>
+            <Section styles={styles.section}>
+              <div styles={styles.stat}>
+                <Text styles={styles.text}>Budget Amount </Text>
+                <Text styles={styles.heading}> Rs.{data?.budgetAmount}</Text>
+              </div>
+              <div styles={styles.stat}>
+                <Text styles={styles.text}>Spent so far for now</Text>
+                <Text styles={styles.heading}> Rs.{data?.totalExpenses}</Text>
+              </div>
+              <div styles={styles.stat}>
+                <Text styles={styles.text}>Remaining</Text>
+                <Text styles={styles.heading}>
+                  {" "}
+                  Rs.{data?.budgetAmount - data?.totalExpenses}
+                </Text>
+              </div>
+            </Section>
+          </Container>
+        </Body>
+      </Html>
+    );
+  }
+}
+
+const styles = {
+  body: {
+    backgroundColor: "#f6f9fc",
+    fontFamily: "-apple-system, sans-serif",
+  },
+  container: {
+    backgroundColor: "#ffffff",
+    margin: "0 auto",
+    padding: "20px",
+    borderRadius: "5px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+  },
+  title: {
+    color: "#1f2937",
+    fontSize: "32px",
+    fontWeight: "bold",
+    textAlign: "center",
+    margin: "0 0 20px",
+  },
+  heading: {
+    color: "#1f2937",
+    fontSize: "20px",
+    fontWeight: "600",
+    margin: "0 0 16px",
+  },
+  text: {
+    color: "#4b5563",
+    fontSize: "16px",
+    margin: "0 0 16px",
+  },
+  section: {
+    marginTop: "32px",
+    padding: "20px",
+    backgroundColor: "#f9fafb",
+    borderRadius: "5px",
+    border: "1px solid #e5e7eb",
+  },
+  statsContainer: {
+    margin: "32px 0",
+    padding: "20px",
+    backgroundColor: "#f9fafb",
+    borderRadius: "5px",
+  },
+  stat: {
+    marginBottom: "16px",
+    padding: "12px",
+    backgroundColor: "#fff",
+    borderRadius: "4px",
+    boxShadow: "0 1px 2px rgba(0, 0, 0, 0.05)",
+  },
+};
