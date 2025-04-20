2f17e058a28958b113fd6d3faf6f8618d49a67f4
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2025-04-20 12:38:39 +0530

    Add transaction backend for creating transaction

---
+++ app/(main)/transaction/_components/transactionForm.jsx
@@ -0,0 +1,13 @@
+"use-client"
+
+import React from 'react'
+
+const AddTransactionForm = () => {
+  return (
+    <div>
+        This is my transaction form
+    </div>
+  )
+}
+
+export default AddTransactionForm;
